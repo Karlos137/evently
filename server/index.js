978f699f42efecb0bc16207c0caa374e8cb824a9
Karel Kvítek <karel.kvitek@seznam.cz>
2020-03-09 14:57:35 +0100

    POST - create event in progress.

--- server/index.js
+++ server/index.js
@@ -17,12 +17,27 @@ app.listen(port, () => {
 //GET - all events
 app.get("/api/events", (req, res) => {
   const sql = "SELECT * FROM events";
+
   db.execute(sql)
     .then(result => {
-      console.log(result[0][0]);
+      console.log(result[0]);
+      res.json(result[0]);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+//POST - create event
+app.post("/api/event", async (req, res) => {
+  console.log(req.body);
+  const sql = `INSERT INTO events (name, date, dateCreated, image, description, location) VALUES ("${req.body.name}", "2020-03-10", "2020-03-09", "${req.body.image}", "Blablab", "Testlocation")`;
+  console.log(sql);
+  db.execute(sql)
+    .then(result => {
       res.json(result);
     })
     .catch(err => {
-      console.log(err);
+      res.send(err);
     });
 });
