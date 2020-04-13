18dd36a09a63f132b12d1f989d807069755685d4
Karel Kvítek <karel.kvitek@seznam.cz>
2020-04-13 12:46:13 +0200

    Neo4j setup created.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("./database");
+const neo4j = require("neo4j-driver");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(express.json());
+
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic(process.env.DATABASE_USER, process.env.DATABASE_PASSWORD)
+);
+const session = driver.session();
 
 app.listen(port, () => {
   console.log(`Listenning on port ${port}...`);
@@ -16,73 +22,12 @@ app.listen(port, () => {
 
 //GET - all groups
 app.get("/api/groups", (req, res) => {
-  const sql = "SELECT * from evently.groups";
+  const cypher = "MATCH (n) RETURN n";
 
-  db.execute(sql)
+  session
+    .run(cypher)
     .then((result) => {
-      res.json(result[0]);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-//GET - all events
-app.get("/api/events", (req, res) => {
-  const sql = "SELECT * FROM evently.events";
-
-  db.execute(sql)
-    .then((result) => {
-      console.log(result[0]);
-      res.json(result[0]);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-//POST - create event
-app.post("/api/event", (req, res) => {
-  const sql = `INSERT INTO evently.events (\`name\`, \`image\`, \`description\`, \`location\`, \`date\`, \`date_created\`, \`created_by\`) VALUES ("${
-    req.body.name
-  }", "${req.body.image}", "${req.body.description}", "${
-    req.body.location
-  }", "${new Date().toJSON().slice(0, 19).replace("T", " ")}", "${new Date(
-    req.body.date
-  )
-    .toJSON()
-    .slice(0, 19)
-    .replace("T", " ")}", "1")`;
-
-  db.execute(sql)
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-//DELETE - delete group
-app.delete("/api/group/:id", (req, res) => {
-  const sql = `DELETE FROM evently.groups WHERE \`id\`="${req.params.id}"`;
-
-  db.execute(sql)
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-//PATCH - group edit
-app.patch("/api/edit/group", (req, res) => {
-  const sql = `UPDATE evently.groups SET \`name\`="${req.body.name}" WHERE \`id\`="${req.body.id}"`;
-
-  db.execute(sql)
-    .then((result) => {
-      res.json(result);
+      res.json(result.records);
     })
     .catch((err) => {
       res.send(err);
